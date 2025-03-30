8b945a0479ff5ed51114e9f8544f6aa2245aa14c
Fernando Fernandes <fernando@parceirogoogle.com.br>
2025-03-30 10:24:24 -0300

    alterando banco

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/register', async (req, res) => {
   try {
